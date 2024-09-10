bd7ef26a31681c27c2fdbe957247e009469f1e92
Steven Kirk <grokys@users.noreply.github.com>
2024-09-10 09:44:46 +0200

    Added behavior.

---
+++ DataGridRowSelectionBinding/Behaviors/DataGridExtensions.cs
@@ -0,0 +1,54 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Reactive;
+
+namespace DataGridRowSelectionBinding.Behaviors;
+
+internal static class DataGridExtensions
+{
+    public static readonly AttachedProperty<bool> SynchronizeSelectionProperty =
+        AvaloniaProperty.RegisterAttached<DataGrid, bool>("SynchronizeSelection", typeof(DataGridExtensions));
+
+    static DataGridExtensions()
+    {
+        SynchronizeSelectionProperty.Changed.Subscribe(
+            new AnonymousObserver<AvaloniaPropertyChangedEventArgs<bool>>(OnSynchronizeSelectionChanged));
+    }
+
+    public static bool GetSynchronizeSelection(DataGrid dataGrid)
+    {
+        return dataGrid.GetValue(SynchronizeSelectionProperty);
+    }
+
+    public static void SetSynchronizeSelection(DataGrid dataGrid, bool value)
+    {
+        dataGrid.SetValue(SynchronizeSelectionProperty, value);
+    }
+
+    private static void OnSynchronizeSelectionChanged(AvaloniaPropertyChangedEventArgs<bool> e)
+    {
+        if (e.Sender is DataGrid dataGrid)
+        {
+            // TODO: Synchronize initial state.
+            if (e.GetNewValue<bool>())
+                dataGrid.SelectionChanged += OnDataGridSelectionChanged;
+            else
+                dataGrid.SelectionChanged -= OnDataGridSelectionChanged;
+        }
+    }
+
+    private static void OnDataGridSelectionChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        foreach (var item in e.AddedItems)
+        {
+            if (item is ISelectable i)
+                i.IsSelected = true;
+        }
+
+        foreach (var item in e.RemovedItems)
+        {
+            if (item is ISelectable i)
+                i.IsSelected = false;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# DataGridRowSelectionBinding
+# Synchronizing DataGrid Row Selection with the ViewModel
+
+This repository contains an example using a XAML behavior to synchronize the selection of rows in a DataGrid with a ViewModel in WPF.
+
+It defines a [`DataGridExtensions.SynchronizeSelection`](DataGridRowSelectionBinding/Behaviors/DataGridExtensions.axaml) attached property that can be set on a `DataGrid`:
+
+```xml
+<DataGrid ItemsSource="{Binding Items}"
+          b:DataGridExtensions.SynchronizeSelection="{Binding SelectedItem}"/>
+```
+
+(see [MainView.axaml](DataGridRowSelectionBinding/Views/MainView.axaml))
+
+When set, the attached property will synchronize the selection of rows in the `DataGrid` with each bound row model. The behavior assumes that the model implements `IsSelectable` and hence has a boolean `IsSelected` property.
